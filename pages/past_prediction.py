a04482f209e3095359d1b9f84aa21eb577990734
baldecoder <mamadoumouctarbalde1@gmail.com>
2023-10-28 16:34:15 +0000

    revert

--- pages/past_prediction.py
+++
@@ -1,3 +0,0 @@
-import streamlit as st
-st.markdown("# Past Predictions 🎈")
-st.sidebar.markdown("# Here you can see all the pas predictions")
--- prediction.py
+++
@@ -1,4 +0,0 @@
-import streamlit as st
-
-st.markdown("Live Prediction")
-st.sidebar.markdown("# here you can do a live predictions 🎈")
