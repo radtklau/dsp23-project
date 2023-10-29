c95490fbaba228ecb6990564de5657c64ab97d30
radtklau <laurids.radtke1@gmail.com>
2023-10-29 19:48:32 +0100

    added training loop

--- model/training.py
+++ model/training.py
@@ -23,3 +23,28 @@ if __name__ == "__main__":
     
     criterion = nn.MSELoss()
     optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.9)
+    
+    training_loss = []
+    
+    # enumerate epochs
+    for epoch in tqdm(range(1000)):
+        running_loss = 0.0
+        # enumerate mini batches
+        for i, (inputs, targets) in enumerate(train_dl):
+            # clear the gradients
+            optimizer.zero_grad()
+            # compute the model output
+            yhat = model(inputs)
+            # calculate loss
+            loss = criterion(yhat, targets)
+            # credit assignment
+            loss.backward()
+            # update model weights
+            optimizer.step()
+            
+            running_loss += loss.item()
+        
+        epoch_loss = running_loss / len(train_dl)
+        training_loss.append(epoch_loss)
+        
+    epochs = range(1, len(training_loss) + 1)
