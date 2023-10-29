dce7f494afabf9359e890afae77eb921fad0d992
radtklau <laurids.radtke1@gmail.com>
2023-10-29 19:31:11 +0100

    implemented dataset class

---
+++ model/dataset.py
@@ -0,0 +1,24 @@
+from torch.utils.data import Dataset
+import numpy as np
+import json
+import torch
+import pandas as pd
+
+class CSVDataset(Dataset):
+    # load the dataset
+    def __init__(self, path):
+        try:
+            with open(path, 'r') as json_file:
+                self.data = pd.read_csv(path)
+        except FileNotFoundError:
+            # The file doesn't exist, so we catch the `FileNotFoundError` exception and initialize an empty dictionary.
+            pass
+
+    # number of rows in the dataset
+    def __len__(self):
+        return len(self.data)
+
+    # get a row at an index
+    def __getitem__(self, idx):
+        sample = self.data.iloc[idx].values
+        return sample
