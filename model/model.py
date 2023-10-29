af6f21ff96833beea90fbcfc9a75dd32ba75159f
radtklau <laurids.radtke1@gmail.com>
2023-10-29 19:39:22 +0100

    implemented initial model architecture

--- model/model.py
+++ model/model.py
@@ -1,1 +1,17 @@
-print("hello world")
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+import torch
+
+#TODO add more layers
+
+class LR_Model(nn.Module):
+    def __init__(self):
+        super(LR_Model, self).__init__()
+        self.layer = nn.Linear(4, 2)  # Three inputs and two outputs
+        self.activation = F.sigmoid
+
+    def forward(self, x):
+        x = x.to(torch.float32)
+        out = self.activation(self.layer(x))
+        return out
