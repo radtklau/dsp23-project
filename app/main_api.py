d1a03945347538e993751462f3d26e4062a68c67
baldecoder <mamadoumouctarbalde1@gmail.com>
2023-11-06 18:07:52 +0000

    remise de mon password dans le fichier

--- app/main_api.py
+++ app/main_api.py
@@ -14,7 +14,7 @@ app = FastAPI()
 model = joblib.load("..\\data\\housepricing.joblib")
 
 ############################################### DATABASE CONNECTION ###########################################
-DATABASE_URL = "postgresql://postgres:Password@localhost:5432/dsp23"
+DATABASE_URL = "postgresql://postgres:Loyaldreambalde11@localhost:5432/dsp23"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
