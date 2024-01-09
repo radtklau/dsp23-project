005ad454bcb5b243bed77aba8c3cf208ce1f531f
radtklau <laurids.radtke1@gmail.com>
2024-01-09 20:24:54 +0100

    fixed some errors and implemented new feature in prediction_job.py to remember already predicted files

--- util/create_folder_A.py
+++ util/create_folder_A.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import os
 from preprocess import preprocess
+from tqdm import tqdm
 
 COLUMNS = ['TotRmsAbvGrd', 'WoodDeckSF', 'YrSold', '1stFlrSF','Foundation','KitchenQual']
 
@@ -10,7 +11,7 @@ if __name__ == "__main__":
     data = preprocess(data)
     # os.mkdir("../data/folder_A")
     
-    for file in range(1000):
+    for file in tqdm(range(1000)):
         num_random_rows = 10
         random_rows = data.sample(n=num_random_rows)
 
